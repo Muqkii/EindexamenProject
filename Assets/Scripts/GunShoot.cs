e1355933417e50aa48a4fe6dd6635def0f912b2f
lukasproject1 <2187633@roc-dev.com>
2025-09-09 13:40:00 +0200

    shooting werkt nu

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -1,42 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GunShoot : MonoBehaviour
 {
     public LayerMask layerMask;
+    public float damage= 10f;
+    public float range = 100f;
 
-    private void Awake()
-    {
-        layerMask= LayerMask.GetMask("Ground","wall");
-    }
+    public Camera fpsCam;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetButtonDown("Fire1"))
         {
 
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Did hit");
-            }
-            else
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.blue);
-                Debug.Log("Did not hit");
-            }
+            Shoot();
 
         }
         
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
     }
 }
