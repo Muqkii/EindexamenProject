9963c35b208ae833945a35e2020d644bad22acbb
lukasproject1 <2187633@roc-dev.com>
2025-09-09 11:25:26 +0200

    raycasting

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GunShoot : MonoBehaviour
 {
+    public LayerMask layerMask;
+
+    private void Awake()
+    {
+        layerMask= LayerMask.GetMask("Ground","wall");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class GunShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did hit");
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.blue);
+                Debug.Log("Did not hit");
+            }
+
+        }
         
     }
 }
