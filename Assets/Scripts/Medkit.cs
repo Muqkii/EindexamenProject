86d6da984ab0c4cb72ef00897494f11b27a76d47
Muqkii <120956662+Muqkii@users.noreply.github.com>
2025-09-18 10:45:06 +0200

    Medkit
    medkit!

---
+++ Assets/Scripts/Medkit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Medkit : MonoBehaviour
+{
+    public int healthGain = 25;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var playerStats = other.gameObject.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.health = healthAdder(playerStats.health);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    int healthAdder(int health)
+    {
+        health += healthGain;
+        if (health > 100)
+        {
+            health = 100;
+        }
+        return (health);
+    }
+}
