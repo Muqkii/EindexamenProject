a13ef63c2462fffb7e57e2fbd07285918486a03d
lukasproject1 <2187633@roc-dev.com>
2025-10-07 17:07:22 +0200

    death screen

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class PlayerStats : MonoBehaviour
     {
         if(health <= 0)
         {
+            SceneManager.LoadScene("DeathScreen");
             //death
         }
     }
